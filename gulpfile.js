72fb48ab380fc4746c50b3f6d9186047da9f0329
Nicolas Lebrun <nclslbrn@gmail.com>
2019-06-28 17:41:46 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,144 @@
+var gulp = require('gulp'),
+    /*You can see dependancies in the package.json */
+    plugins = require('gulp-load-plugins')({
+        pattern: '*'
+    }),
+    fs = require('fs'),
+    del = require('del'),
+    path = require('path'),
+    browserSync = require('browser-sync'),
+    modRewrite = require('connect-modrewrite');
+
+/* Paths to Dev and dist environnement for path flexibility*/
+var devPaths = {
+    base: 'src/',
+    tmpl: 'src/index.pug',
+    sass: 'src/style.scss',
+    styles: 'dist/style.css',
+    script: 'src/script.js',
+};
+
+var distPaths = {
+    base: 'dist/',
+    tmpl: 'dist/index.html',
+    styles: 'dist/style.css',
+    sass: 'dist/',
+    script: 'dist/script.js',
+};
+var additionalFiles = ['src/d3.min.js', 'src/data.json'];
+
+// Browser synchronisation
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: distPaths.base,
+            proxy: 'localhost',
+            port: 80,
+            open: true,
+            startPath: 'index.html',
+        },
+        //  browser: 'Safari'
+    });
+});
+
+// Compile pug templates to html pages
+gulp.task('pug', function() {
+    return gulp.src(devPaths.tmpl)
+        .pipe(plugins.pug({
+            pretty: true,
+        }))
+        .pipe(plugins.prettify({
+            indent_size: 4,
+            preserve_newlines: true
+        }))
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+
+// Duplicate main.js file and minify it in the distPath (js/)
+gulp.task('js', function() {
+    return gulp.src(devPaths.script)
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.concat('script.js'))
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        //  .pipe(plugins.uglify())
+        .pipe(plugins.sourcemaps.write('.'))
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Compile sass main file to a css file in distPath (css/)
+gulp.task('sass', function() {
+    return gulp.src(devPaths.sass)
+        .pipe(plugins.sourcemaps.init(distPaths.base))
+        .pipe(plugins.sass({
+            includePaths: ['css'],
+            onError: browserSync.notify,
+            precision: 10,
+        }))
+        .pipe(plugins.autoprefixer(
+            ['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+                cascade: true
+            }
+        ))
+        .pipe(plugins.cssbeautify({
+            indent: '  ',
+            autosemicolon: true
+        }))
+        .pipe(plugins.sourcemaps.write(distPaths.base))
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// Duplicate css files to the distPath (css/main.min.css)
+gulp.task('css', function() {
+    return gulp.src(distPaths.styles)
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(plugins.cleanCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Add lib to js folder into webPath (js/)
+gulp.task('lib', function() {
+    gulp.src(additionalFiles)
+        .pipe(gulp.dest(distPaths.base))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// Remove hidden files
+gulp.task('cleanup', function() {
+    del('**/.DS_Store');
+});
+
+gulp.task('clean', function() {
+    del(distPaths.base + '*');
+});
+
+gulp.task('default', ['browserSync', 'css', 'pug', 'js', 'sass', 'lib'], function() {
+    gulp.watch(devPaths.tmpl, ['pug']);
+    gulp.watch(devPaths.script, ['js']);
+    gulp.watch(devPaths.sass, ['sass']);
+    gulp.watch(distPaths.styles, ['css']);
+    gulp.watch(additionalFiles, ['lib']);
+    gulp.watch(devPaths.src + '**/.DS_Store', ['clean']);
+});
---
+++ src/script.js
@@ -0,0 +1,144 @@
+function wrap(text, width) {
+    text.each(function() {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            y = text.attr("y"),
+            dy = parseFloat(text.attr("dy")),
+            tspan = text.text(null)
+            .append("tspan")
+            .attr("x", text.attr('x'))
+            .attr("y", y)
+            .attr("dy", dy + "em");
+
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan")
+                    .attr("x", text.attr('x'))
+                    .attr("y", y)
+                    .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                    .text(word);
+            }
+        }
+    });
+}
+
+
+// Get the data
+d3.json("data.json").then(function(data) {
+
+    var width = window.innerWidth,
+        height = window.innerHeight / 2,
+        margin = {
+            top: 20,
+            right: 30,
+            bottom: 50,
+            left: 40
+        },
+        zoneHeight = 48,
+        circleSize = 16,
+        eventWidth = 84,
+        xScale = d3.scaleBand(),
+        yScale = d3.scaleBand(),
+        xAxis = d3.axisBottom(xScale),
+        zoneMinX = d3.min(data.zone, function(d) {
+            return d['begin'];
+        }),
+        zoneMaxX = d3.max(data.zone, function(d) {
+            return d['end'];
+        }),
+        eventMinX = d3.min(data.event, function(d) {
+            return d['date'];
+        }),
+        eventMaxX = d3.min(data.event, function(d) {
+            return d['date'];
+        }),
+        globalMinX = Math.min(zoneMinX, eventMinX),
+        globalMaxX = Math.max(zoneMaxX, eventMaxX);
+
+    xScale.domain(d3.range(globalMinX - 4, globalMaxX + 1));
+    xScale.rangeRound([0, width]);
+    yScale.domain(data.zone.map(d => d.pos)).range([0, height]);
+
+    var zoneX = function(d, i) {
+            return xScale(d['begin']);
+        },
+        zoneY = function(d) {
+            return yScale(d.pos);
+        },
+        zoneW = function(d, i) {
+            return xScale(d['end']) - xScale(d['begin'])
+        },
+        zoneH = zoneHeight;
+
+    var eventX = function(d, i) {
+            return xScale(d['date']);
+        },
+        eventY = function(d) {
+            return yScale(d.pos);
+        };
+
+    var chart = d3.select('.timeline')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.left + margin.right)
+        .append('g')
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add x axis
+    chart.append("g")
+        .attr("class", "axis axis-x");
+
+    chart.select('.axis-x')
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add zones
+    var zone = chart.selectAll('.timeline-zone')
+        .data(data.zone)
+        .enter().append('g')
+        .classed('timeline-zone', true);
+
+    var rect = zone.append('rect')
+        .attr('x', zoneX)
+        .attr('y', zoneY)
+        .attr('width', zoneW)
+        .attr('height', zoneH);
+
+    var label = zone.append('text')
+        .attr('x', 0)
+        .attr('y', zoneY)
+        .attr('transform', 'translate(0, ' + zoneHeight / 2 + ')')
+        .attr('dy', '.35em')
+        .text(function(d) {
+            return d.label;
+        });
+
+    var event = chart.selectAll('.timeline-event')
+        .data(data.event)
+        .enter().append('g')
+        .classed('timeline-event', true);
+
+    var dot = event.append('circle')
+        .attr('cx', eventX)
+        .attr('cy', eventY)
+        .attr('r', circleSize);
+
+    var info = event.append('text')
+        .attr('x', eventX)
+        .attr('y', eventY)
+        .attr('transform', 'translate(0, ' + circleSize / 2 + ')')
+        .attr('dy', '.35em')
+        .text(function(d) {
+            return d.label;
+        }).call(wrap, eventWidth);
+
+
+});
