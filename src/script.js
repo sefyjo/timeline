05312e3437db1bb7364db53ef13b0eaf1ae2fd2a
Nicolas Lebrun <nclslbrn@gmail.com>
2019-07-05 12:41:10 +0200

    Set the last zone bigger than others

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 // Get the data
 d3.json('data.json').then(function(data) {
 
-    var width = 8000,
+    let width = 8000,
         height = 1120,
         start = -7000,
         colors = ['#b4c8da', '#787d80'],
         //colors = ['#303952', '#546de5'],
-        zoneHeight = height / data.zone.length,
+        zoneHeight = (height / data.zone.length),
         zoneCornerWidth = zoneHeight,
         zoneMinX = start,
         zoneMaxX = d3.max(data.zone, function(d) {
@@ -21,19 +21,23 @@ d3.json('data.json').then(function(data) {
         globalMinX = Math.min(zoneMinX, eventMinX),
         globalMaxX = Math.max(zoneMaxX, eventMaxX);
 
-    var steps = [start, 1350, 1700, 1915, 2000, globalMaxX];
-    var stepsWidth = width / (steps.length - 1);
-    var stepsValue = [0, stepsWidth, stepsWidth * 2, stepsWidth * 3, stepsWidth * 4, width];
 
-    var xScale = d3.scaleLinear()
+    // Set scale interpolation limit 
+    const steps = [start, 1350, 1700, 1915, 2000, globalMaxX];
+    const stepsWidth = width / (steps.length - 1);
+    // Set scale interpolation width
+    const stepsValue = [0, stepsWidth, stepsWidth * 2, stepsWidth * 3, stepsWidth * 4, width];
+    const xScale = d3.scaleLinear()
         .domain(steps)
         .range(stepsValue);
 
-    var yScale = d3.scaleBand()
-        .domain(data.zone.map(d => d.pos))
-        .range([0, height]);
+    const yScale = d3.scaleLinear()
+        .domain([0, data.zone.length - 1, data.zone.length])
+        .range([0, (height - (zoneHeight * 3)), height]);
 
-    var xAxis = d3.axisBottom(xScale)
+
+
+    let xAxis = d3.axisBottom(xScale)
         .ticks(data.event.length) // read data from event data
         .tickValues(d3.set(data.event.map(function(d) {
             return d.date
@@ -41,9 +45,9 @@ d3.json('data.json').then(function(data) {
         .tickSize(height) // full height line
         .tickFormat(d3.format("d")); // remove comma from thousand delimeter
 
-    var colorInterpolation = d3.quantize(d3.interpolateHcl(colors[0], colors[1]), data.zone.length);
+    let colorInterpolation = d3.quantize(d3.interpolateHcl(colors[0], colors[1]), data.zone.length);
 
-    var zoneX = function(d, i) {
+    let zoneX = function(d, i) {
             return xScale(start);
         },
         zoneY = function(d) {
@@ -55,7 +59,7 @@ d3.json('data.json').then(function(data) {
         zoneH = zoneHeight;
 
     // rect with bottom right angle cutted (x - rect height)
-    var polyZone = function(d, i) {
+    let polyZone = function(d, i) {
         return [
             [
                 0,
@@ -84,7 +88,7 @@ d3.json('data.json').then(function(data) {
         ];
     };
     // distribute event on zones/thematics
-    var eventX = function(d, i) {
+    let eventX = function(d, i) {
             return xScale(d['date']);
         },
         eventY = function(d) {
@@ -94,18 +98,18 @@ d3.json('data.json').then(function(data) {
     /**
      * TIMEMLINE BACK
      */
-    var chartBack = d3.select('#timeline--background')
+    let chartBack = d3.select('#timeline--background')
         .attr('width', width)
-        .attr('height', height)
+        .attr('height', (height + zoneHeight * 2))
         .append('g');
 
     // Add zones
-    var zone = chartBack.selectAll('.timeline-zone')
+    let zone = chartBack.selectAll('.timeline-zone')
         .data(data.zone.reverse())
         .enter().append('g')
         .classed('timeline-zone', true);
 
-    var poly = zone.append('polygon')
+    let poly = zone.append('polygon')
         .attr('x', zoneX)
         .attr('y', zoneY)
         .attr('points', polyZone)
@@ -125,7 +129,7 @@ d3.json('data.json').then(function(data) {
         .style('text-anchor', 'middle')
         .attr('transform', 'rotate(-90) translate(-20,-7)');
 
-    var label = zone.append('text')
+    let label = zone.append('text')
         .attr('x', 8)
         .attr('y', zoneY)
         .attr('transform', 'translate(0, ' + zoneHeight / 2 + ')')
@@ -136,32 +140,32 @@ d3.json('data.json').then(function(data) {
     /**
      * TIMEMLINE FRONT
      */
-    var chartFront = d3.select('#timeline--content')
+    let chartFront = d3.select('#timeline--content')
         .style('width', width)
         .style('height', height);
 
-    var eventsBlocks = [];
+    let eventsBlocks = [];
 
     for (let e = 0; e < data.event.length - 1; e++) {
 
         if (data.event[e].title && data.event[e].date && data.event[e].pos) {
 
-            var event = chartFront.append('div')
+            let event = chartFront.append('div')
                 .datum(data.event[e])
                 .classed('timeline--content--event', true)
                 .style('left', eventX)
                 .style('top', eventY);
 
-            var eventContainer = event.append('div')
+            let eventContainer = event.append('div')
                 .attr('class', 'event-inner');
 
-            var title = eventContainer.append('h3')
+            let title = eventContainer.append('h3')
                 .text(function(d) {
                     return d.title;
                 });
 
             if (data.event[e].text) {
-                var text = eventContainer.append('p')
+                let text = eventContainer.append('p')
                     .text(function(d) {
                         if (d.text) {
                             return d.text;
@@ -169,7 +173,7 @@ d3.json('data.json').then(function(data) {
                     });
             }
             if (data.event[e].link) {
-                var link = eventContainer.append('a')
+                let link = eventContainer.append('a')
                     .attr('href', data.event[e].link)
                     .text('+');
             }
